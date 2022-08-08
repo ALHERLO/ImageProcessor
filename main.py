abaedb885bb3725b57ec110d01daa6bfd1aa8eda
alanhernandez <alherlo23@gmail.com>
2022-08-07 20:23:33 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from PIL import Image
+
+doggo = Image.open('doggo.jpg')
+stamp = Image.open('stamp.jpg').resize(doggo.size).convert(doggo.mode)
+blend = Image.blend(doggo, stamp, 0.1)
+print(stamp.mode)
+
+
+blend.show()
