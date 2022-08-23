485270dbae8f21dc79db17ae4c20066569cfb7c9
alanhernandez <alherlo23@gmail.com>
2022-08-22 21:45:29 -0700

    Changes in water mark density

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from PIL import Image
 
 doggo = Image.open('doggo.jpg')
 stamp = Image.open('stamp.jpg').resize(doggo.size).convert(doggo.mode)
-blend = Image.blend(doggo, stamp, 0.5)
+blend = Image.blend(doggo, stamp, 0.7)
 print(stamp.mode)
 
 
