2282eabff32eb0d076c5e3b9de6a3de9ed306e1b
alanhernandez <alherlo23@gmail.com>
2022-10-07 22:27:15 -0700

    Changes in water mark density

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-from PIL import Image
+import tkinter as tk
+from PIL import Image, ImageTk
 
-doggo = Image.open('doggo.jpg')
-stamp = Image.open('stamp.jpg').resize(doggo.size).convert(doggo.mode)
-blend = Image.blend(doggo, stamp, 0.5)
-print(stamp.mode)
+# doggo = Image.open('doggo.jpg')
+# stamp = Image.open('stamp.jpg').resize(doggo.size).convert(doggo.mode)
+# blend = Image.blend(doggo, stamp, 0.5)
+# print(stamp.mode)
+# blend.show()
+
+class Window:
+    def __init__(self, master):
+        # self.image=tk.PhotoImage(file = "doggo.jpg")
+        self.pillow_image = Image.open("doggo2.jpg") # 👉🏽 Read Image with pillow
+        self.image = ImageTk.PhotoImage(self.pillow_image) # 👉🏽 Convert to compatible image with Tk
+        self.label=tk.Label(master, image =self.image)
+        self.label.pack(padx=5, pady=5)
 
 
-blend.show()
+if(input("Mark stamp on image y/n?")) == 'y':
+    print('it is same')
+root = tk.Tk()
+window = Window(root)
+root.mainloop()
+
